2e881448746f561e0359c2cd79bf6c96e0ec2210
DABAYO <dbayo2000@yahoo.com>
2018-01-18 14:56:49 -0500

    final commit

--- script.js
+++ script.js
@@ -1,54 +1,71 @@
-$("button").on("click", function() {
-    var toon = $(this).attr("cartoon");
-  
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      toon + "&api_key=nQe1Lc5ctWi9AXcvYbLPKZbgMxPKwhF9&limit=10";
-  
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
+var buttonArray = ["Adventure Time", "Hey Arnold", "The Jetsons", "The Fairly OddParents", "Animaniacs"];
 
-      .then(function(response) {
-        var results = response.data;
-  
-        for (var i = 0; i < results.length; i++) {
-  
-          if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
 
-            var toonGif = $("<div class='item'>");
-  
-            var rating = results[i].rating;
-  
-            var p = $("<p>").text("Rating: " + rating);
-  
-            var toonImage = $("<img>");
-  
-            toonImage.attr("src", results[i].images.fixed_height.url);
-  
-            toonGif.append(p);
-            toonGif.append(toonImage);
-  
-            $("#cartoonGifs").prepend(toonGif);
-          }
+function renderButtons() {
+  buttonArray.forEach(function (val) {
+    var button = $("<button type='button'>");
+    button.addClass("btn btn-primary cartButton");
+    button.attr("data-cartoon", val);
+    button.text(val);
+    $("#cartoon-buttons").append(button);
+  });
+}
+renderButtons();
+
+$("#submit").on("click", function () {
+  var userInput = $('#cartoonInput').val();
+  buttonArray.push(userInput);
+  $('#cartoonInput').val("");
+  $("#cartoon-buttons").empty();
+  renderButtons();
+})
+
+$("#cartoon-buttons").on("click", ".cartButton", function () {
+  $("#cartoonGifs").empty();
+  // var toon = $(this).attr("cartoon");
+  var cartoonVal = $(this).attr("data-cartoon");
+  var apiKey = "nQe1Lc5ctWi9AXcvYbLPKZbgMxPKwhF9";
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    cartoonVal + "&api_key=" + apiKey + "&limit=10";
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    .then(function (response) {
+      console.log(response);
+
+      var results = response.data;
+      for (var i = 0; i < results.length; i++) {
+        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+          var toonGif = $("<div class='item'>");
+          var rating = results[i].rating;
+          var p = $("<p>").text("Rating: " + rating);
+          var toonImage = $("<img>");
+          toonImage.attr("class", "gifImage");
+          toonImage.attr("src", results[i].images.fixed_height.url);
+          toonImage.attr("data-still", results[i].images.original_still.url);
+          toonImage.attr("data-animate", results[i].images.fixed_height.url);
+          toonImage.attr("data-state", "animate");
+          
+          toonGif.append(p);
+          toonGif.append(toonImage);
+          $("#cartoonGifs").prepend(toonGif);
         }
-      });
-  });
+      }
+    });
+});
 
-$("cartoon-buttons").append("Some appended text.");
 
-$(".gif").on("click", function() {
-    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-    var state = $(this).attr("data-state");
-    
-    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-    // Then, set the image's data-state to animate
-    // Else set src to the data-still value
-    if (state === "still") {
-      $(this).attr("src", $(this).attr("data-animate"));
-      $(this).attr("data-state", "animate");
-    } else {
-      $(this).attr("src", $(this).attr("data-still"));
-      $(this).attr("data-state", "still");
-    }
-  });
+
+$("#cartoonGifs").on("click", ".gifImage", function () {
+
+  var state = $(this).attr("data-state");
+
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});
